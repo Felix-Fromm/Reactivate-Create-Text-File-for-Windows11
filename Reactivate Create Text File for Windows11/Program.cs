987a7ede89b2d252279ae012361afc367b7f6713
Felix <ThisIsFrommherz@gmail.com>
2022-11-15 17:39:19 +0100

    Projektdateien hinzufügen.

---
+++ Reactivate Create Text File for Windows11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Win32;
+public class Reactivator
+{
+    public static void Main()
+    {
+        Console.WriteLine("Do you want to get the old 'Create Textfile' back? Y/N");
+        if (Console.ReadLine().ToLower() == "y")
+        {
+            DoWork();
+            Console.WriteLine("Finish");
+        } else
+        {
+            Console.WriteLine("Then no i guess");
+        }
+    }
+
+    public static void DoWork()
+    {
+        RegistryKey key;
+        key = Registry.ClassesRoot.CreateSubKey(".txt").CreateSubKey("ShellNew");
+        key.SetValue("", "NullFile");
+        key.Close();
+
+        RegistryKey key2;
+        key2 = Registry.ClassesRoot.CreateSubKey("txtfilelegacy");
+        key2.SetValue("", "Text File");
+        key2.Close();
+    }
+}
